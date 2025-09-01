9bf7672875779a8699d651103a0fca0a473a36b7
o3cloudng <o3cloudng@gmail.com>
2025-09-01 14:11:35 +0100

    Config vercel python3.9

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+pip install -r reuirements.txt
+python3.9 manage.py collectstatic --noinput
+# #!/bin/bash
 
 # Install Python dependencies
-echo "Installing Python dependencies..."
-pip install -r requirements.txt
+# echo "Installing Python dependencies..."
+# pip install -r requirements.txt
 
-# Run Django migrations
-echo "Running Django migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+# # Run Django migrations
+# echo "Running Django migrations..."
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py migrate --noinput
 
-# Collect static files
-echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput --clear
+# # Collect static files
+# echo "Collecting static files..."
+# python3.9 manage.py collectstatic --noinput --clear
 
-echo "Build process completed."
+# echo "Build process completed."
