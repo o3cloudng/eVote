e0d6c959a4849619a8127a15fa622a47671fd28c
o3cloudng <o3cloudng@gmail.com>
2025-09-01 14:20:54 +0100

    Config vercel pip3

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-
-# Install Python dependencies
-echo "Installing Python dependencies..."
 pip3 install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
 
-# Run Django migrations
-echo "Running Django migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+# # Install Python dependencies
+# echo "Installing Python dependencies..."
+# pip3 install -r requirements.txt
 
-# Collect static files
-echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput --clear
+# # Run Django migrations
+# echo "Running Django migrations..."
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py migrate --noinput
 
-echo "Build process completed."
+# # Collect static files
+# echo "Collecting static files..."
+# python3.9 manage.py collectstatic --noinput
+
+# echo "Build process completed."
